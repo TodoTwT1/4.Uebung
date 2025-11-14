f928f286d34112e2b858e010ff07b6fa5613eb81
Todo <todotwt99@gmail.com>
2025-11-14 16:15:54 +0100

    to transfer to other pc

---
+++ src/h1/H1_main.java
@@ -0,0 +1,28 @@
+package h1;
+public class H1_main {
+    static void main(String[] args){
+
+        int zone=4;
+        double price=20;
+
+        switch (zone){
+            case 1: price=2;;
+            break;
+            case 2: price=2.35;
+            break;
+            case 3: price=2.85;
+            break;
+            case 4: price=2.85;
+            break;
+            case 5: price=3.55;
+            break;
+            default:price=4;
+        }
+
+System.out.println(price);
+
+
+
+
+    }
+}
---
+++ src/h2/H2_main.java
@@ -0,0 +1,24 @@
+package h2;
+
+public class H2_main {
+    public static void main(String[] args){
+
+        boolean x=true;
+        boolean y=true;
+        boolean a=false;
+        boolean b=false;
+        boolean c=true;
+        int input=0;
+
+        switch (input){
+            case 10: x=true;
+            case 11: y=true;
+            case 1: y=true
+        }
+
+
+
+
+
+    }
+}
